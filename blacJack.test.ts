4472fbb1b2737365840583c5cf7e46277626855d
Jean-Charles GOULEAU <jean-charles.gouleau@savoye.com>
2024-05-07 20:09:27 +0200

    Changes by marc-gavanier

--- blacJack.test.ts
+++ blacJack.test.ts
@@ -14,11 +14,33 @@ type Deck = [...typeof VALUES, ...typeof VALUES, ...typeof VALUES, ...typeof VAL
 
 const DECK: Deck = [...VALUES, ...VALUES, ...VALUES, ...VALUES]
 
-type PlayingPosition = {}
+type PlayingPosition = {
+    availableMoney: number
+} | {}
+
+type Model<TName extends string, TValues> = TValues & { [modelKey in `is${Capitalize<TName>}`]: true };
+
+type Bet = Model<'Bet', { min: number, max: number }>;
+
+const throwBetError = (bet: Omit<Bet, 'isBet'>): Bet => {
+    throw new Error(`Bet with value min: ${bet.min} and max: ${bet.max} is invalid`);
+};
+
+const isValiBet = (bet: Omit<Bet, 'isBet'>): bet is Bet => bet.min >= 0 && bet.max > bet.min;
+
+const Bet = (bet: Omit<Bet, 'isBet'>): Bet => (isValiBet(bet) ? bet : throwBetError(bet))
+
+
+type BlackJackConfiguration = {
+    playingPositionCount: PlayingPositionCount,
+    decksCount: DecksCount,
+    bet: Bet
+}
 
 type BlackJack = {
-    playingPositions: PlayingPosition[]
-    cards: Card[]
+    playingPositions: PlayingPosition[],
+    cards: Card[],
+    bet: Bet
 }
 
 type Shuffler = (cards: Card[]) => Card[]
@@ -38,23 +60,47 @@ const testShuffler: Shuffler = (cards: Card[]) => {
     return cards;
 }
 
+const invite = (player: PlayingPosition) => (game: BlackJack): BlackJack => {
+    const availableIndex: number | null = game.playingPositions.findIndex((playingPosition: PlayingPosition) => !playingPosition.hasOwnProperty('availableMoney'));
+    
+    if (availableIndex == null) throw new Error('There is no more available playing position');
+
+    const playingPositions = [...game.playingPositions];
+    playingPositions[availableIndex] = player
+    
+    return ({
+        ...game,
+        playingPositions
+    })
+}
+
 // todo: min bet
 // todo: max bet
-// todo: add id to playing position
 // todo: add player max money
+// todo: add id to playing position ????
 // todo: add Bet function that takes amount and playing position id => returns BlackJack with ready player
 // todo: distribute cards for every bets...
 
 describe('blackJack game ', () => {
 
     test('should create a game between 5 and 9 playingPosition and between 1 and 8 decks 52 cards', () => {
-        const game = BlackJack(testShuffler)(5, 2)
+        const game = BlackJack(testShuffler)({
+            playingPositionCount: 5,
+            decksCount: 2,
+            bet: Bet({min: 5, max: 100})
+        })
         expect(game.playingPositions.length).toBe(5);
         expect(game.cards.length).toBe(104);
+        expect(game.bet.min).toBe(5);
+        expect(game.bet.max).toBe(100);
     })
 
     test('should create a game with 1 shuffeled deck', () => {
-        const game = BlackJack(testShuffler)(5, 1)
+        const game = BlackJack(testShuffler)({
+            playingPositionCount: 5,
+            decksCount: 1,
+            bet: Bet({min: 5, max: 100})
+        })
         const expectedDesk: Card[]= [
             'j', 'k', 'q', '5', '3', '5', '5', '6', '4', '10',
             '7', '10', '7', 'k', '4', '7', '7', 'j', '4', 'as', '10', '9', '3', '8',
@@ -64,12 +110,32 @@ describe('blackJack game ', () => {
 
         expect(game.cards).toStrictEqual(expectedDesk);
     })
+
+    test('should invite two players', () => {
+        const game = BlackJack(testShuffler)({
+            playingPositionCount: 5,
+            decksCount: 1,
+            bet: Bet({min: 5, max: 100})
+        })
+
+        const gameWithOnePlayer = invite({availableMoney: 500})(game)
+        const gameWithTwoPlayers = invite({availableMoney: 600})(gameWithOnePlayer)
+
+        expect(gameWithTwoPlayers.playingPositions).toStrictEqual([
+            {availableMoney: 500},
+            {availableMoney: 600},
+            {},
+            {},
+            {},
+        ])
+    })
 })
 
 
-const BlackJack = (shuffler: Shuffler) => (playingPositionCount: PlayingPositionCount, decksCount: DecksCount): BlackJack => {
+const BlackJack = (shuffler: Shuffler) => (blackJackConfiguration: BlackJackConfiguration): BlackJack => {
     return {
-        playingPositions: new Array(playingPositionCount).fill({}),
-        cards: shuffler(new Array(decksCount).fill(DECK).flat())
+        playingPositions: new Array(blackJackConfiguration.playingPositionCount).fill({}),
+        cards: shuffler(new Array(blackJackConfiguration.decksCount).fill(DECK).flat()),
+        bet: blackJackConfiguration.bet
     }
 }
